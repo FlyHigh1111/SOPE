9e673f653a14da87f4e24bcbb2b05e7c4bb9584c
Angela <up201806781@g.uporto.pt>
2021-04-12 14:24:00 +0100

    funçao thread

--- MP2/src/client.c
+++ MP2/src/client.c
@@ -11,6 +11,9 @@
 
 #define BUFSIZE 1024
 
+
+pthread_mutex_t lock;
+
 struct Arguments
 {
     size_t nsecs; // number of seconds
@@ -41,12 +44,31 @@ void ParseArguments(int argc, char *argv[], struct Arguments *args)
     args->nsecs = atoi(argv[2]);
     args->public_fifo = argv[3];
 }
+void* fthread(){
+    int fp;
+    char nfifopriv[200];
+    //cria ffifo privado
+    pthread_mutex_lock(&lock);
+    sprintf(nfifopriv,"/tmp/%d.%ld",getpid(),pthread_self());
+    printf("nfifo: %s \n",nfifopriv);
+    mkfifo(nfifopriv, 0666);//cria fifo privado
+    pthread_mutex_unlock(&lock);
+    //construir mensagem que vai ser enviado servidor:gerar numero 0-9
+    //write mensagem no fifo publico (fd fornecido com argumento da funçao)
+    fp=open(nfifopriv,O_NONBLOCK,O_WRONLY);//futuro tirar nonblock
+    printf("p:%d tid:%ld \n",fp,pthread_self());
+    //read resposta do servidor 
+    //publicar resultado 
+    //fechar fifoprivado 
+    //eliminar fifoprivado
+    
+}
 
 int main(int argc, char *argv[], char *envp[])
 {
     struct Arguments args;
     ParseArguments(argc, argv, &args);
-    int fd = mkfifo(args.public_fifo, S_IWUSR | S_IWOTH | S_IWGRP);
+    int fd = mkfifo(args.public_fifo, S_IWUSR | S_IWOTH | S_IWGRP);//sair porque este é o fifo publico cujo ja esta criado e nome é passado por parametro, nao é o cliente a criar o fifo
 
     if(fd == -1){
         fprintf(stderr, "Error creating FIFO");
@@ -81,6 +103,19 @@ int main(int argc, char *argv[], char *envp[])
 
         i++;
     }
+    
+    /*pthread_t id[20];
+
+    pthread_mutex_init(&lock, NULL);
+    for(int i=1;i<=20;i++){
+        pthread_create(&id[i], NULL, fthread, NULL);
+
+    }
+    for(int i=1;i<=20;i++){
+        pthread_join(id[i], NULL);
+    }
+    pthread_mutex_destroy(&lock);
+    */
      
      close(open_fd);
 
