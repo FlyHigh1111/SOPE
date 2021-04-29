9ff29e5bde1177192811ffa5a9ba39a1adf5cff1
Angela <up201806781@g.uporto.pt>
2021-04-29 10:58:44 +0100

    gavup

--- MP2/src/client.c
+++ MP2/src/client.c
@@ -165,7 +165,7 @@ int main(int argc, char *argv[], char *envp[])
         //ns = time(NULL);
         th++;
     }
-sleep(1);
+usleep(500000);
 char str[256];
 for(int i=0;i<th;i++){
     snprintf(str,256,"/tmp/%d.%ld",getpid(),tid[i]);
