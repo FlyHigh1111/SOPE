895035323022a42c8cadbfc215c586939806f2e4
Angela <up201806781@g.uporto.pt>
2021-05-10 11:43:48 +0100

    parse arguments server

--- MP2/src/server.c
+++ MP2/src/server.c
@@ -28,7 +28,7 @@ void ParseArguments(int argc, char *argv[], struct Arguments *args)
 
     }
 if(argc==6){
-    if((!isNumeric(argv[4]) || atoi(argv[4])<0) && argc==6){
+    if(!isNumeric(argv[4]) || atoi(argv[4])<0){
    
         fprintf(stderr, "Buffer size should be a positive integer!\n");
         exit(1);
