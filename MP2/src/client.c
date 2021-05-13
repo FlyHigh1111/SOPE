d9032aef94986572771e47aa2c284004abf24df5
Angela <up201806781@g.uporto.pt>
2021-05-13 12:07:03 +0100

    server

--- MP2/src/client.c
+++ MP2/src/client.c
@@ -1,4 +1,18 @@
 #include "./includes/client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#include <ctype.h>
 
 void ParseArguments(int argc, char *argv[], struct Arguments *args)
 {
--- MP2/src/includes/client.h
+++ MP2/src/includes/client.h
@@ -1,6 +1,20 @@
 #pragma once
 
 #include "./common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#include <ctype.h>
 
 #define CLIENT -1
 #define MAX_THREADS 10000
