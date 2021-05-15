d5754f2408ca41ee6a5a98f2941156a7e0fe1c6a
Angela <up201806781@g.uporto.pt>
2021-05-15 13:11:34 +0100

    server

--- MP2/src/server.c
+++ MP2/src/server.c
@@ -94,7 +94,7 @@ void* ThreadHandlerProd(void *arguments){
     while(queueIsFull(&queue,args->nmax));
     pthread_mutex_lock(&lock);
     pushbackqueue(&queue,args->armazem, response_message,args->nmax);
-        printf("produtor entrou queue %d %d \n",queue.primeiro,args->armazem[queue.ultimo].tskres);
+        printf("produtor entrou queue %d %d %d \n",queue.primeiro,args->armazem[queue.ultimo].tskres,args->armazem[queue.ultimo].rid);
         
     
     pthread_mutex_unlock(&lock);
