1fd9541177258a6adacadbfdf62cfb5ed52b1d99
Angela <up201806781@g.uporto.pt>
2021-05-18 19:41:20 +0100

    message

--- MP2/src/server.c
+++ MP2/src/server.c
@@ -39,14 +39,14 @@ void ParseArguments(int argc, char *argv[], struct Arguments *args)
             exit(1);
         }
         args->buffer_size=atoi(argv[4]);
-        strcpy(args->public_fifo,argv[5]);
+        args->public_fifo = argv[5];
     }
     args->nsecs = atoi(argv[2]);
 
     if(argc==4)
     {
         args->buffer_size = BUFF_SIZE;
-        strcpy(args->public_fifo,argv[3]);      
+        args->public_fifo = argv[3];
     }
 }
 void sigAlrmHandlerS(int signum)
@@ -63,27 +63,30 @@ void* ThreadHandlerCons(void *arguments)
 
     while(!finish || !queueIsEmpty(&queue))
     {
-      //case if queue is not empty
-      if(!queueIsEmpty(&queue))
-        {         
-          //access the cloud for the next message and pops it 
-          topQueue(&queue, args->cloud, &response_message);
-          popQueue(&queue, args->cloud, args->nmax);
-        
-          //writes response in the private fifo
-          snprintf(private_fifo, BUFFER_SIZE, "/tmp/%d.%ld", response_message.pid, response_message.tid);
-          int fd_private_fifo = open(private_fifo, O_WRONLY);
-          write(fd_private_fifo, &response_message, sizeof(response_message));
+        //case if queue is not empty
+        //printf("consumidor entrou no ciclo: %p %d %d \n",&queue,queue.first,queue.last);
+        if(!queueIsEmpty(&queue))
+        {
+            //printf("consumidor entrou na fila \n");
+            //access the cloud for the next message and pops it 
+            topQueue(&queue, args->cloud, &response_message);
+            popQueue(&queue, args->cloud, args->nmax);
+            
+            //writes response in the private fifo
+            snprintf(private_fifo, BUFFER_SIZE, "/tmp/%d.%ld", response_message.pid, response_message.tid);
+            int fd_private_fifo = open(private_fifo, O_WRONLY);
+            write(fd_private_fifo, &response_message, sizeof(response_message));
 
-          //constructs the message/log to print to stdout
-          struct Log log;
-          log.i = response_message.rid;
-          log.t = response_message.tskload;
-          log.pid = response_message.pid;
-          log.tid = response_message.tid;
-          log.res = response_message.tskres;
-          log.oper = "TSKDN";
-          WriteLog(log);
+            //constructs the message/log to print to stdout
+            struct Log log;
+            log.i = response_message.rid;
+            log.t = response_message.tskload;
+            log.pid = response_message.pid;
+            log.tid = response_message.tid;
+            log.res = response_message.tskres;
+            log.oper = "TSKDN";
+            WriteLog(log);
+
         }
         
         //case if queue is empty
@@ -91,8 +94,6 @@ void* ThreadHandlerCons(void *arguments)
     }
     pthread_exit(NULL);
 }
-
-
 void* ThreadHandlerProd(void *arguments)
 {
     struct Message response_message;
@@ -135,11 +136,6 @@ int main(int argc,char** argv)
 {
     struct Arguments args;
    
-/*if (argc==1) {
-    args.buffer_size=10;
-    strcpy(args.public_fifo,"testefifo");
-    args.nsecs = 10;
-}*/  
     struct ArgsThreadsProducer argsthsprod[MAX_THREADS];
     struct ArgsThreadsConsumer argsthscon;
     struct Message request_message;
@@ -181,7 +177,7 @@ int main(int argc,char** argv)
         //reads requests coming from the public fifo
         if((j = read(fd_publicfifo, &request_message, sizeof(struct Message))) > 0)
         {   
-            //constructs the message/log to print to stdout   
+        //constructs the message/log to print to stdout   
             log.i = request_message.rid;
             log.t = request_message.tskload;
             log.pid = request_message.pid;
@@ -189,7 +185,6 @@ int main(int argc,char** argv)
             log.res = request_message.tskres;
             log.oper = "RECVD";
             WriteLog(log);
-
             argsthsprod[th].rid=request_message.rid;
             argsthsprod[th].tid=request_message.tid;
             argsthsprod[th].pid =request_message.pid;
@@ -197,26 +192,29 @@ int main(int argc,char** argv)
             argsthsprod[th].tskload=request_message.tskload;
             argsthsprod[th].cloud=cloud;
             argsthsprod[th].nmax=args.buffer_size;
+            //printf("pid %d tid %ld tskload %d  th: %d\n",request_message.pid,request_message.tid,request_message.tskload,th);
             pthread_create(&tid[th],NULL,&ThreadHandlerProd,&argsthsprod[th]);
             th++;
        
 
         }     
     }
-    close(fd_publicfifo);
+      close(fd_publicfifo);
+    //printf("saiu \n");
     //main thread waits for the producer (k>=1) and consumer (k=0) threads to finish 
-    for(int k = th; k >= 0; k--)
+    for(int k = th; k>=0; k--)
     {
         pthread_join(tid[k], NULL);
     }
-    //close(fd_publicfifo);
-    
+
+  
     unlink(args.public_fifo);
     free(cloud);
 
     return 0;
 
 }
+
 
 
 
